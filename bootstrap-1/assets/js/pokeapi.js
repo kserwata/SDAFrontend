0836c63c6a6c4ef75181c25582420aeb311eca99
Kamil Serwata <k.serwata@live.com>
2020-10-01 20:11:46 +0200

    add AJAX call to pokeapi

--- bootstrap-1/assets/js/pokeapi.js
+++ bootstrap-1/assets/js/pokeapi.js
@@ -1,6 +1,30 @@
 function onSubmit() {
-    const pokemonName = $("[name=pokemonName]").val();
-    $("#submitButton").removeClass("btn-primary").addClass("btn-warning");
-    alert(`You entered: ${pokemonName}`);
+    showLoader(() => {
+        $(".pokemon-info").slideUp(500, () => {
+            const pokemonName = $("[name=pokemonName]").val();
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+                success: (data) => {
+                    $("#name").html(data.name);
+                    $("#stats").html("");
+                    for(let stat of data.stats) {
+                        $("#stats").append(`<span>${stat.stat.name}: ${stat.base_stat}</span><br/>`);
+                    }
+                    $(".pokemon-info").slideDown();
+                    hideLoader();
+                }
+            }); 
+        });
+    })
     return false;
+}
+
+function showLoader(callback) {
+    $(".preloader").fadeIn(500, () => {
+        callback();
+    })
+}
+
+function hideLoader() {
+    $(".preloader").fadeOut();
 }
