d2dcbf20d8b6e1239f246f2a695d3afecac8cccd
Kamil Serwata <k.serwata@live.com>
2020-10-01 19:34:27 +0200

    change form handler to JQuery

--- bootstrap-1/assets/js/pokeapi.js
+++ bootstrap-1/assets/js/pokeapi.js
@@ -1,5 +1,6 @@
 function onSubmit() {
-    const pokemonName = document.getElementsByName("pokemonName")[0].value;
+    const pokemonName = $("[name=pokemonName]").val();
+    $("#submitButton").removeClass("btn-primary").addClass("btn-warning");
     alert(`You entered: ${pokemonName}`);
     return false;
 }
